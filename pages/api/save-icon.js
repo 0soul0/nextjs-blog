efe63612779067ce652d946bd91db74b137c81a9
Shu ya <sevideo6666@gmail.com>
2023-05-23 17:38:14 +0800

    success

--- pages/api/save-icon.js
+++ pages/api/save-icon.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import fs from 'fs';
 export default async function handler(req, res) {
-    if (req.method !== 'POST') return
+    // if (req.method !== 'POST') return
     const { iconData, title } = req.body
     const folderName = title;
     try {
@@ -12,15 +12,32 @@ export default async function handler(req, res) {
         console.error(err);
     }
 
-    iconData.forEach((e, index) => {
+    // iconData.forEach((e, index) => {
 
-        axios({
-            url: e.iconURL,
-            responseType: 'stream'
-        }).then(response => {
-            response.data.pipe(fs.createWriteStream(folderName + "/" + index + ".png"))
-        })
-        // res.status(200).json({ data: iconData[0].iconURL });
-    })
+    //     axios({
+    //         url: e.iconURL,
+    //         responseType: 'stream'
+    //     }).then(response => {
+    //         response.data.pipe(fs.createWriteStream(folderName + "/" + index + ".png"))
+    //     })
+
+    // })
+
+    const requests = iconData.map(url => axios({
+        url: url.iconURL,
+        responseType: 'stream'
+    }));
+
+    axios.all(requests)
+        .then(axios.spread((...responses) => {
+            responses.forEach((e, index) => {
+                e.data.pipe(fs.createWriteStream(folderName + "/" + index + ".png"))
+            })
+            res.status(200).json({ data: "success" });
+        }))
+        .catch(error => {
+            // 处理错误
+            console.error('Error:', error.message);
+        });
 }
 
